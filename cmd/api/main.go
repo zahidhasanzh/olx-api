70583b07d6fe8251c2566d8c26cff9128d2cdd84
Zahid Hassan <61347323+zahidhasanzh@users.noreply.github.com>
2026-08-31 11:41:08 +0600

    add server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status": "ok"}`))
+	})
 
-func main(){
-	fmt.Println("olx api server is running!!!")
+	srv := http.Server{
+		Addr:         ":8090",
+		Handler:      mux,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 30,
+		IdleTimeout:  time.Second * 60,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("Server failed %v", err)
+	}
+
 }
