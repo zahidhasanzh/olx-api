69c5984d07d9223323622bd8ce5ccbe58739a689
Zahid Hassan <61347323+zahidhasanzh@users.noreply.github.com>
2026-09-01 01:26:37 +0600

    change status message

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -19,7 +19,7 @@ func main() {
 	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"status": "ok"}`))
+		w.Write([]byte(`{"status": "okay"}`))
 	})
 
 	srv := http.Server{
