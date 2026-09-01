ca50a2acfa4d8e412b6f0560ef8090ca90698c00
Zahid Hassan <61347323+zahidhasanzh@users.noreply.github.com>
2026-09-01 10:47:53 +0600

    move healthz to handlers

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/zahidhasanzh/olx-api/internal/config"
+	"github.com/zahidhasanzh/olx-api/internal/handlers"
 )
 
 func main() {
@@ -16,11 +17,7 @@ func main() {
 	fmt.Println("starting olx server...")
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"status": "okay"}`))
-	})
+	mux.HandleFunc("GET /healthz", handlers.Healthz)
 
 	srv := http.Server{
 		Addr:         ":" + cnf.Port,
